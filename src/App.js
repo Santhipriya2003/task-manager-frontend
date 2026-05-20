210dc7f3f2558c4df4d72d6cbd5a0d850b8586b1
santhi <santhipriyaveligarapu@gmail.comgit config --global user.name>
2026-05-20 16:50:52 +0530

    fix import paths in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
  import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Login";
+import Login from "./pages/Login.jsx";
 import Dashboard from "./Dashboard";
 
 function App() {
